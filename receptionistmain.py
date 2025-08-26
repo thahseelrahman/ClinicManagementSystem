53cb0e97dbb19629a66243c13a616269b184d02c
THAHSEEL <thahseelchullikulavan@gmail.com>
2025-08-26 17:32:26 +0530

    filename changed

---
+++ receptionistmain.py
@@ -0,0 +1,44 @@
+from db.db_connection import DBConnection
+from lib.ReceptionistManagement import ReceptionmanagementLib
+def main():
+    while True:
+        # db = DBConnection()
+        # conn = db.get_connection()
+        print("\n==========PATIENT MANAGEMENT MENU==========")
+        print("1. ADD PATIENT")
+        print("2. GET ALL PATIENTS")
+        # print("3. GET PATIENT")
+        # print("4. UPDATE PATIENT")
+        # print("5. DELETE PATIENT")
+        print("\n========APPOINTMENT MANAGEMENT========")
+        print("6. SCHEDULE APPOINTMENT")
+        print("7. GET ALL APPOINTMENTS")
+        # print("8. EDIT APPOINTMENT")
+        # print("9. CANCEL APPOINTMENT")
+        # print("10. GET APPOINTMENT")
+        print("11. EXIT")
+        choice = input("Enter your choice : ")
+        if choice == "1":
+            ReceptionmanagementLib.add_patient()
+        elif choice == "2":
+            ReceptionmanagementLib.get_all_patients()
+        elif choice == "3":
+            ReceptionmanagementLib.get_patient()
+        elif choice == "4":
+            ReceptionmanagementLib.update_patient()
+        elif choice == "5":
+            ReceptionmanagementLib.delete_patient()
+        elif choice == "6":
+            ReceptionmanagementLib.schedule_appoinment()
+        elif choice == "7":
+            ReceptionmanagementLib.get_all_appointments()
+        elif choice == "8":
+            ReceptionmanagementLib.update_appointment()
+        elif choice == "9":
+            ReceptionmanagementLib.cancel_appointment()
+        elif choice == "10":
+            ReceptionmanagementLib.get_appointment()
+        elif choice == "11":
+            break
+        else:
+            print("Invalid choice!! Try again!!")
